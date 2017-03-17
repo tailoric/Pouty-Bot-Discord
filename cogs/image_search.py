f1dea14a958561adee3b99d4f9a8b7d017a367ed
tailoric <eric.schneider96@gmail.com>
2017-03-17 14:25:07 +0100

    feat(image_search) added tineye and saucenao reverse search
    .sauce command and .tineye now as reverse image search command available
    Lookup either via uploaded file or image link

--- cogs/image_search.py
+++ cogs/image_search.py
@@ -7,17 +7,64 @@ import json
 class Search:
     """Reverse image search commands"""
 
+
     def __init__(self, bot):
         self.bot = bot
-        self.session = aiohttp.ClientSession()
-        self.session2 = aiohttp.ClientSession()
+        self.iqdb_session = aiohttp.ClientSession()
+        self.dans_session = aiohttp.ClientSession()
+        self.sauce_session = aiohttp.ClientSession()
+        self.tineye_session = aiohttp.ClientSession()
 
-    def _tag_to_title(self,tag):
+    async def _danbooru_api(self, link):
+        """
+        looks up information about the image on danbooru
+        :param link: must be a valid danbooru link
+        :return: characters, artist, copyright (franchise)
+        """
+
+        # json file with api key and user name for danbooru
+        # Structure:
+        # {
+        #  "user": "username",
+        #  "api_key": "ValidApiKey"
+        # }
+        auth_file_path = 'data/danbooru/danbooru.json'
+        with open(auth_file_path, 'r') as file:
+            data = json.load(file)
+            user = data['user']
+            api_key = data['api_key']
+        auth = aiohttp.BasicAuth(user, api_key)
+        characters, artist, franchise = None, None, None
+        async with self.dans_session.get('{}.json'.format(link), auth=auth) as response:
+            if response.status == 200:
+                json_dump = await response.json()
+                if json_dump['tag_count_character'] > 0:
+                    characters = self._tag_to_title(json_dump['tag_string_character'])
+                if json_dump['tag_count_artist'] > 0:
+                    artist = self._tag_to_title(json_dump['tag_string_artist'])
+                if json_dump['tag_count_copyright'] > 0:
+                    franchise = self._tag_to_title(json_dump['tag_string_copyright'])
+                return characters, artist, franchise
+            else:
+                await self.bot.reply("\n HTTP Error occured with following Status Code:{}".format(response.status))
+
+    def __unload(self):
+        self.iqdb_session.close()
+        self.dans_session.close()
+        self.sauce_session.close()
+        self.tineye_session.close()
+
+
+    def _tag_to_title(self, tag):
         return tag.replace(' ', ', ').replace('_', ' ').title()
 
     @commands.command(pass_context=True)
-    async def source(self, ctx, link=None):
-        """Search IQDB for Source of the link or uploaded image"""
+    async def iqdb(self, ctx, link=None):
+        """Search IQDB for source of an image on danbooru
+        if no danbooru link is found it returns the best match
+        usage:  .iqdb <link> or
+                .iqdb on image upload comment
+        """
         file = ctx.message.attachments
         if link is None and not file:
             await self.bot.say('Message didn\'t contain Image')
@@ -27,7 +74,7 @@ class Search:
                 url = link
             else:
                 url = file[0]['proxy_url']
-            async with self.session.post(url='https://iqdb.org', data={'url': url}) as response:
+            async with self.iqdb_session.post(url='https://iqdb.org', data={'url': url}) as response:
                 if response.status == 200:
                     soup = BeautifulSoup(await response.text(), 'html.parser')
                     # This is for the no relevant matches case
@@ -58,29 +105,69 @@ class Search:
                     if not danbooru_found:
                         await  self.bot.reply('<{}>'.format(best_match))
 
-    async def _danbooru_api(self, link):
-        with open('data/danbooru/danbooru.json','r') as file:
-            data = json.load(file)
-            user = data['user']
-            api_key = data['api_key']
-            auth = aiohttp.BasicAuth(user,api_key)
-            characters, artist, franchise = None, None, None
-            async with self.session2.get('{}.json'.format(link),auth=auth) as response:
+    @commands.command(pass_context=True)
+    async def sauce(self, ctx, link=None, similarity=80):
+        """
+        reverse image search via saucenao
+       usage:   .sauce <image-link> <similarity (in percent)> or
+                .sauce on image upload comment <similarity (in percent)>
+        """
+        file = ctx.message.attachments
+        if link is None and not file:
+            await self.bot.say('Message didn\'t contain Image')
+        else:
+            await self.bot.type()
+            if file:
+                url = file[0]['proxy_url']
+                similarity = link
+            else:
+                url = link
+            async with self.sauce_session.get('http://saucenao.com/search.php?url={}'.format(url)) as response:
+                source = None
                 if response.status == 200:
-                    json_dump = await response.json()
-                    if json_dump['tag_count_character'] > 0:
-                        characters = self._tag_to_title(json_dump['tag_string_character'])
-                    if json_dump['tag_count_artist'] > 0:
-                        artist = self._tag_to_title(json_dump['tag_string_artist'])
-                    if json_dump['tag_count_copyright'] > 0:
-                        franchise = self._tag_to_title(json_dump['tag_string_copyright'])
-                    return characters, artist, franchise
-                else:
-                    await self.bot.reply("\n HTTP Error occured with following Status Code:{}".format(response.status))
+                    soup = BeautifulSoup(await response.text(), 'html.parser')
+                    for result in soup.select('.resulttablecontent'):
+                        if float(similarity) > float(result.select('.resultsimilarityinfo')[0].contents[0][:-1]):
+                            break
+                        else:
+                            if result.select('a'):
+                                source = result.select('a')[0]['href']
+                                await self.bot.reply('<{}>'.format(source))
+                                return
+                    if source is None:
+                        await self.bot.reply('No source over the similarity threshold')
 
-    def __unload(self):
-        self.session.close()
-        self.session2.close()
+    @commands.command(pass_context=True)
+    async def tineye(self, ctx, link=None):
+        """
+        reverse image search using tineye
+        usage:  .tineye <image-link> or
+                .tineye on image upload comment
+        """
+        file = ctx.message.attachments
+        if link is None and not file:
+            await self.bot.say('Message didn\'t contain Image')
+        else:
+            await self.bot.type()
+            if file:
+                url = file[0]['proxy_url']
+            else:
+                url = link
+            async with self.tineye_session.get('https://tineye.com/search/?url={}'.format(url)) as response:
+                soup = BeautifulSoup(await response.text(), 'html.parser')
+                pages = []
+                image_link = None
+                for hidden in soup.find(class_='match').select('.hidden-xs'):
+                    if hidden.contents[0].startswith('Page:'):
+                        pages.append('<{}>'.format(hidden.next_sibling['href']))
+                    else:
+                        image_link = hidden.a['href']
+            message = '\n**Pages:** '
+            message += '\n**Pages:** '.join(pages)
+            if image_link is not None:
+                message += '\n**direct image:** <{}>'.format(image_link)
+            await self.bot.reply(message)
+
 
 def setup(bot):
     bot.add_cog(Search(bot))
