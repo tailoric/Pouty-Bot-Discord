6136fcd37b6b32c3d8ec4df4835834473e9410e9
Saikimo <eric.schneider96@gmail.com>
2017-04-16 12:55:56 +0000

    fix(wuxia) only send when status code == 200

--- cogs/wuxia.py
+++ cogs/wuxia.py
@@ -20,32 +20,34 @@ class Wuxia:
         while not self.bot.is_closed:
             try:
                 async with self.wuxia_session.get(self.url) as response:
-                    soup = BeautifulSoup(await response.text(), 'html.parser')
-                    if not os.path.isfile(self.entries_db):
-                        file = open(self.entries_db,'w')
-                        file.close()
+                    if response.status == 200:
+                        soup = BeautifulSoup(await response.text(), 'html.parser')
+                        if not os.path.isfile(self.entries_db):
+                            file = open(self.entries_db,'w')
+                            file.close()
 
-                with open(self.entries_db,'r+',encoding='utf-8') as f:
-                    content = f.read()
-                    for item in soup.find_all('item'):
-                        title = item.title.contents[0]
-                        link = item.link.contents[0]
-                        pubdate = item.pubdate.contents[0]
-                        match = re.search(r'ISSTH Chapter (\d+)', title)
-                        line = '{} | {} | {}\n'.format(
-                            title,
-                            link,
-                            pubdate
-                        )
+                        with open(self.entries_db,'r+',encoding='utf-8') as f:
+                            content = f.read()
+                            for item in soup.find_all('item'):
+                                title = item.title.contents[0]
+                                link = item.link.contents[0]
+                                pubdate = item.pubdate.contents[0]
+                                match = re.search(r'ISSTH Chapter (\d+)', title)
+                                line = '{} | {} | {}\n'.format(
+                                    title,
+                                    link,
+                                    pubdate
+                                )
 
-                        if match and line not in content:
-                            f.write(line)
-                            await self.bot.send_message(self.channel,link)
+                                if match and line not in content:
+                                    f.write(line)
+                                    await self.bot.send_message(self.channel,link)
                 await asyncio.sleep(10)
 
             except Exception as e:
-                user = discord.Object(id='287707546624458763')
+                user = discord.Object(id='300764599068786698')
                 await self.bot.send_message(user, str(e))
+                await asyncio.sleep(60)
                 continue
 
 def setup(bot):
