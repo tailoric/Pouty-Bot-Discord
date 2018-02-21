1d4bf61d933168eb53e0aced47f7e1a7eab7f2f5
tailoric <eric.schneider96@gmail.com>
2018-02-21 20:13:08 +0100

    feat(administration) allow moderators and admins to reload cogs

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -1,14 +1,36 @@
 from discord.ext import commands
 import discord.utils
 
+
 def is_owner_check(message):
     return message.author.id == '134310073014026242'
+
 
 def is_shadow_check(message):
     return message.author.id == '191782132002193410'
 
+
 def is_shadow():
     return commands.check(lambda ctx: is_shadow_check(ctx.message))
 
+
 def is_owner():
     return commands.check(lambda ctx: is_owner_check(ctx.message))
+
+
+def is_owner_or_admin_check(message):
+   return message.author.server_permissions.administrator or is_owner_check(message)
+
+
+def is_owner_or_moderator_check(message):
+    if is_owner_check(message):
+        return True
+    return message.author.server_permissions.manage_server
+
+
+def is_owner_or_admin():
+    return commands.check(lambda ctx: is_owner_or_admin_check(ctx.message))
+
+
+def is_owner_or_moderator():
+    return commands.check(lambda ctx: is_owner_or_moderator_check(ctx.message))
