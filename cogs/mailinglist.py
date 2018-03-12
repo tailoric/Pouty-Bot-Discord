f3ac7bf124156a08b048bf60e213bc425b4a04e5
tailoric <eric.schneider96@gmail.com>
2018-03-12 13:35:17 +0100

    make mailinglist runnable
    fix various indentation and syntax issues, add mlist group command

--- cogs/mailinglist.py
+++ cogs/mailinglist.py
@@ -1,26 +1,19 @@
-from discord.ext import commands
-import discord
 import aiohttp
 import json
 import os
-import datetime
-from dateutil import parser
-import asyncio
-import re
-import traceback
 from .utils import checks
-import logging
+from discord.ext import commands
 
 """
 Schema of Subs
 [
-	"subs": {
-		"listName": [1112, 32],
-		"listName2": [123, 453]
-	},
-	"users": {
-		someUserId: ["listName", listName2"]
-	}
+    "subs": {
+        "listName": [1112, 32],
+        "listName2": [123, 453]
+    },
+    "users": {
+        someUserId: ["listName", listName2"]
+    }
 ]
 """
 
@@ -31,62 +24,65 @@ class Danbooru:
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
-		
-		# Make sure directory to store json is good, and then read a file if it exists
+
+        # Make sure directory to store json is good, and then read a file if it exists
         self.init_directories()
-		self.jsonDir = 'data/mailinglist/data.json'
-		with open(self.jsonDir, 'r') as f:
-			self.json = json.load(f)
+        self.jsonDir = 'data/mailinglist/subs/data.json'
+        with open(self.jsonDir, 'r') as f:
+            self.json = json.load(f)
 
     def __unload(self):
-		return
+        return
 
     def init_directories(self):
         if not os.path.exists('data/mailinglist'):
             os.mkdir('data/mailinglist')
         if not os.path.exists('data/mailinglist/subs/'):
             os.mkdir('data/mailinglist/subs')
-		if not os.file.exists('data/mailinglist/subs/data.json/'):
-		    newJson = open('data.json','w')
-			newJson.write('[]');
-			file.close()
-			
-	def writeUpdatedList(self):
-		try:
-			with open(self.jsonDir, 'w') as f:
-				json.dump(self.json, f)
-		except Exception as e:
-		   await self.bot.say('Error while persisting sub change: `{}`'.format(repr(e)))
-		   raise e
+        if not os.path.exists('data/mailinglist/subs/data.json'):
+            with open('data/mailinglist/subs/data.json', 'w') as newJson:
+                newJson.write('[]')
 
+    async def writeUpdatedList(self):
+        try:
+            with open(self.jsonDir, 'w') as f:
+                json.dump(self.json, f)
+        except Exception as e:
+           await self.bot.say('Error while persisting sub change: `{}`'.format(repr(e)))
+           raise e
+
+    @commands.group(pass_context = True)
+    async def mlist(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await self.bot.say("use `.help mlist`")
     @mlist.command(pass_context=True)
     async def sub(self, ctx, list):
         """
         subscribe to provided list
-        list: list that will be subscribed to 
+        list: list that will be subscribed to
         """
-		message = ctx.message
-		requesterID = message.author.id
-		
-		# Make sure list exists
-		# If it does add the user only if they are not already in the list
-		if list in subs:
-			for userID in self.json["subs"][list]:
-				if userID == requesterID:
-					await self.bot.say('You are already in %(list).' % list)
-					return
-			
-			# Here is where we actually add the user to the list
-			self.json["subs"][list].append(requesterID)
-			self.json["users"][requesterID].append(list)
-			writeUpdatedList()
-			await self.bot.say('You have been added to %(list).' % list)
-			return
-		else:
-			await self.bot.say('List %(list) doesn\'t exist. Create it with `mlist create`.' % list)
-			return
+        message = ctx.message
+        requesterID = message.author.id
 
-			
+        # Make sure list exists
+        # If it does add the user only if they are not already in the list
+        if list in self.subs:
+            for userID in self.json["subs"][list]:
+                if userID == requesterID:
+                    await self.bot.say('You are already in %(list).' % list)
+                    return
+
+            # Here is where we actually add the user to the list
+            self.json["subs"][list].append(requesterID)
+            self.json["users"][requesterID].append(list)
+            self.writeUpdatedList()
+            await self.bot.say('You have been added to %(list).' % list)
+            return
+        else:
+            await self.bot.say('List %(list) doesn\'t exist. Create it with `mlist create`.' % list)
+            return
+
+
 
     @mlist.command(pass_context=True)
     async def unsub(self, ctx, list):
@@ -94,54 +90,54 @@ class Danbooru:
         unsubscribe from given list
         list: list that will be unsubscribed from
         """
-		message = ctx.message
-		requesterID = message.author.id
-		
-		# Make sure list exists
-		# If it does add the user only if they are not already in the list
-		if list in subs:
-			for index, userID in enumerate(self.json["subs"][list]):
-				if userID == requesterID:
-					self.json["users"][requesterID].remove(list)
-					self.json["subs"][list].remove(requesterID)
-					writeUpdatedList()
-					await self.bot.say('You have been removed from %(list).' % list)
-					return
-			
-			await self.bot.say('You are not in list %(list).' % list)
-			return
-		else:
-			await self.bot.say('List %(list) doesn\'t exist. Create it with `mlist create`.' % list)
-			return
-	
-	
+        message = ctx.message
+        requesterID = message.author.id
+
+        # Make sure list exists
+        # If it does add the user only if they are not already in the list
+        if list in self.subs:
+            for index, userID in enumerate(self.json["subs"][list]):
+                if userID == requesterID:
+                    self.json["users"][requesterID].remove(list)
+                    self.json["subs"][list].remove(requesterID)
+                    self.writeUpdatedList()
+                    await self.bot.say('You have been removed from %ds(list).' % list)
+                    return
+
+            await self.bot.say('You are not in list %ds(list).' % list)
+            return
+        else:
+            await self.bot.say('List %(list) doesn\'t exist. Create it with `mlist create`.' % list)
+            return
+
+
     @mlist.command(pass_context=True)
     async def broadcast(self, ctx, list, message):
         """
         Send a message to the given list
         list: list that will be broadcasted to
-		message: message that will be sent
+        message: message that will be sent
         """
-		message = ctx.message
-		requesterID = message.author.id
-		
-		# Make sure list exists
-		# If it does add the user only if they are not already in the list
-		if list in subs:
-			for userID in self.json["subs"][list]:
-				await self.bot.say('<@{}> says {} on list {}.\n{}'.format(requesterID, message, list, pingList(self.json["subs"][list])))
-			return
-		else:
-			await self.bot.say('List %(list) doesn\'t exist. Create it with `mlist create`.' % list)
-			return
+        message = ctx.message
+        requester_id= message.author.id
 
-	# Generate a string with pings for all the userIDs in the list
-	def pingList(self, list):
-		"""
+        # Make sure list exists
+        # If it does add the user only if they are not already in the list
+        if list in self.subs:
+            for userID in self.json["subs"][list]:
+                await self.bot.say('<@{}> says {} on list {}.\n{}'.format(requester_id, message, list, self.pingList(self.json["subs"][list])))
+            return
+        else:
+            await self.bot.say('List %(list) doesn\'t exist. Create it with `mlist create`.' % list)
+            return
+
+    # Generate a string with pings for all the userIDs in the list
+    def pingList(self, list):
+        """
         Generate a string with pings for all the userIDs in the list
         list: list of usersIDs
-		"""
-		return ' '.join(list(map(lambda userID: '<@{}>'.format(userID))))
+        """
+        return ' '.join(list(map(lambda userID: '<@{}>'.format(userID))))
 
     @mlist.command(pass_context=True)
     async def list(self, ctx):
@@ -149,29 +145,13 @@ class Danbooru:
         list all subscribed lists
         """
         message = ctx.message
-		listRequester = message.author.id
-		
-		subList = ', '.join([str(x) for x in self.json["users"][listRequester]])
-		if (subList.length() == 0):
-			await self.bot.say(subList);
+        list_requester = message.author.id
+
+        subList = ', '.join([str(x) for x in self.json["users"][list_requester]])
+        if len(subList) == 0:
+            await self.bot.say(subList)
         else:
             await self.bot.reply('You aren\'t subscribed to any lists')
-			
-	@mlist.command(pass_context=True)
-    @checks.is_owner()
-    async def listAll(self, ctx):
-        """
-        list all subs
-        """
-        message = ctx.message
-		listRequester = message.author.id
-		
-		subList = ', '.join([str(x) for key in self.json["subs"])
-		
-		if (subList.length() == 0):
-			await self.bot.say(subList);
-        else:
-            await self.bot.reply('There are no lists')
 
     @mlist.command()
     async def restart(self):
@@ -180,6 +160,23 @@ class Danbooru:
         """
         self.__unload()
         setup(self.bot)
+
+    @mlist.command(pass_context=True)
+    @checks.is_owner()
+    async def listAll(self, ctx):
+        """
+        list all subs
+        """
+        message = ctx.message
+        list_requester = message.author.id
+
+        subList = ', '.join([str(x) for x in self.json["subs"]])
+
+        if len(subList) == 0:
+            await self.bot.say(subList)
+        else:
+            await self.bot.reply('There are no lists')
+
 
 def setup(bot):
     bot.add_cog(Danbooru(bot))
