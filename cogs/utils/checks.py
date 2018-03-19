6fc34010ab36f3ded5414365aa7b5f8efa82b5c3
tailoric <eric.schneider96@gmail.com>
2018-03-19 21:53:49 +0100

    feat(owner) stop hard coding the owner
    get the owner from the data/credentials.json file

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -1,18 +1,11 @@
 from discord.ext import commands
-import discord.utils
+import json
 
 
 def is_owner_check(message):
-    return message.author.id == '134310073014026242'
-
-
-def is_shadow_check(message):
-    return message.author.id == '191782132002193410'
-
-
-def is_shadow():
-    return commands.check(lambda ctx: is_shadow_check(ctx.message))
-
+    with open('data/credentials.json', 'r') as f:
+        credentials = json.load(f)
+        return message.author.id == credentials['owner']
 
 def is_owner():
     return commands.check(lambda ctx: is_owner_check(ctx.message))
