cbbf30f1da7e289554ed681241e2b907d643fc74
tailoric <eric.schneider96@gmail.com>
2019-01-11 14:10:36 +0100

    feat(filter) filter the default gif sites
    tenor and giphy have their own commands, delete anything posted
    from those sites immediately

---
+++ cogs/filter.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+import json
+import re
+import os
+from .utils.checks import is_owner_or_moderator
+
+class Filter:
+    """
+    filters messages and removes them
+    """
+
+    def __init__(self, bot,):
+        self.bot = bot
+        self.filter_file_path = "data/filter.json"
+
+        if os.path.exists(self.filter_file_path):
+            with open(self.filter_file_path, "r") as filter_file:
+                try:
+                    settings = json.load(filter_file)
+                    self.allowed_channel = self.bot.get_channel(settings['channel_id'])
+                except json.JSONDecodeError:
+                    self.allowed_channel = None
+        else:
+            open(self.filter_file_path, 'w').close()
+            self.allowed_channel = None
+
+    async def on_message(self, message):
+        contains_giphy_or_tenor_link_regex = re.compile("https://(media\.)?(tenor|giphy)?.com/")
+        match = contains_giphy_or_tenor_link_regex.match(message.content)
+        if match and not message.channel == self.allowed_channel:
+            await self.bot.delete_message(message)
+
+    @is_owner_or_moderator()
+    @commands.command(name="filter_exception", pass_context=True)
+    async def setup_exception_channel(self, ctx):
+        """
+        sets up channel as exception for filtering giphy and tenor links
+        """
+        channel = ctx.message.channel
+        self.allowed_channel = channel
+        with open(self.filter_file_path, 'w') as filter_file:
+            settings = {"channel_id" : channel.id}
+            json.dump(settings, filter_file)
+        await self.bot.say("channel {} setup as exception channel".format(channel.mention))
+def setup(bot):
+    bot.add_cog(Filter(bot))
--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -1,9 +1,13 @@
 from discord.ext import commands
 from discord.utils import find
+from discord.ext.commands.errors import BadArgument
+
 
 class RoleConverter(commands.Converter):
 
-
     async def convert(self):
         server = self.ctx.message.server
-        return find(lambda r: r.name.lower() == self.argument, server.roles)
+        role = find(lambda r: r.name.lower() == self.argument, server.roles)
+        if role is None:
+            raise BadArgument('Role {} not found'.format(self.argument))
+        return role
