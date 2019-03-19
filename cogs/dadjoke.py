053036a9da044914ba84c68252ccea4d1b8974fe
tailoric <eric.schneider96@gmail.com>
2019-03-19 20:05:42 +0100

    feat(dadjoke) small april's fool joke

---
+++ cogs/dadjoke.py
@@ -0,0 +1,23 @@
+import re
+class Dadjoke:
+    def __init__(self, bot):
+        self.bot = bot
+        self.result = None
+
+    def check_for_dadjoke(self, message):
+        return message.content.lower().startswith("hi") and self.result.group(2) in message.content
+
+    async def on_message(self, message):
+        result = re.match("^([Ii]\s?'?a?m)\s*(.*)", message.content)
+        if result:
+            self.result = result
+            msg = await self.bot.wait_for_message(check=self.check_for_dadjoke, timeout=60)
+            new_name = msg.content[2:30]
+            if len(msg.content) > 32:
+                new_name += "..."
+            await self.bot.change_nickname(msg.author, new_name)
+
+
+
+def setup(bot):
+    bot.add_cog(Dadjoke(bot))
