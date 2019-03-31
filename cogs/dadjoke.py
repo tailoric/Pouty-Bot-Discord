cfc7d6c5a1383b808a20161d596a9091c753bbed
Eric Schneider <eric.schneider96@gmail.com>
2019-03-31 19:06:40 -0400

    fix(dadjoke) stop users to trigger the message event themselves

--- cogs/dadjoke.py
+++ cogs/dadjoke.py
@@ -3,14 +3,18 @@ class Dadjoke:
     def __init__(self, bot):
         self.bot = bot
         self.result = None
+        self.user = None
 
     def check_for_dadjoke(self, message):
-        return message.content.lower().startswith("hi") and self.result.group(2) in message.content
+        return message.content.lower().startswith("hi") and self.result.group(2) in message.content and message.author.id != self.user.id
 
     async def on_message(self, message):
+        if message.content is None:
+            return
         result = re.match(".*([Ii]\s?'?a?m)\s*(.*)", message.content)
         if result:
             self.result = result
+            self.user = message.author
             msg = await self.bot.wait_for_message(check=self.check_for_dadjoke, timeout=60)
             new_name = msg.content[2:30]
             if len(msg.content) > 32:
