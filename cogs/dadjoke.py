8e678a0fd63db1eb78cf495019f79b1c3fb51ea9
tailoric <eric.schneider96@gmail.com>
2019-04-09 21:45:14 +0200

    feat(dadjoke) extend to react to hello and mute user making joke

--- cogs/dadjoke.py
+++ cogs/dadjoke.py
@@ -1,4 +1,5 @@
 import re
+import time
 class Dadjoke:
     def __init__(self, bot):
         self.bot = bot
@@ -6,7 +7,7 @@ class Dadjoke:
         self.user = None
 
     def check_for_dadjoke(self, message):
-        return message.content.lower().startswith("hi") and self.result.group(2) in message.content and message.author.id != self.user.id
+        return re.match("^([Hh]i|[hH]ello)\s+.*", message.content) and self.result.group(2) in message.content and message.author.id != self.user.id
 
     async def on_message(self, message):
         if message.content is None:
@@ -18,12 +19,17 @@ class Dadjoke:
             msg = await self.bot.wait_for_message(check=self.check_for_dadjoke, timeout=60)
             if msg == None:
                 return
-            new_name = msg.content[2:30]
+            new_name = "  ".join(msg.content.split()[1:])
             if len(msg.content) > 32:
-                new_name += "..."
+                new_name = new_name[:28] + "..."
+            await self._mute_user(msg.author)
             await self.bot.change_nickname(msg.author, new_name)
 
-
+    async def _mute_user(self, user):
+        admin_cog = self.bot.get_cog("Admin")
+        if(admin_cog):
+            await self.bot.add_roles(user,admin_cog.mute_role)
+            admin_cog.mutes.append({"user": user.id, "unmute_ts": int(time.time() + 60)})
 
 def setup(bot):
     bot.add_cog(Dadjoke(bot))
