40df87d01fee3be19b609755287b04c74a447a5c
tailoric <eric.schneider96@gmail.com>
2019-06-20 21:42:39 +0200

    fix(penis) use greedy member transformer for getting users

--- cogs/penis.py
+++ cogs/penis.py
@@ -1,6 +1,7 @@
 import random
 from discord.ext import commands
 from discord.ext.commands import Bot
+import discord
 from .utils.dataIO import DataIO
 
 class Penis(commands.Cog):
@@ -15,30 +16,22 @@ class Penis(commands.Cog):
                 break
 
     @commands.command(pass_context=True)
-    async def penis(self, ctx, *, users: str = None):
+    async def penis(self, ctx, members: commands.Greedy[discord.Member]):
         """accurately measure a user's penis size or compare the penis size of multiple users"""
         if ctx.message.channel != self.allowed_channel:
             await ctx.send(f"Please use the following channel: <#{self.allowed_channel.id}>")
             return
-        if users is None:
-            message = ctx.message
-            seed = message.author.id
-            random.seed(seed)
+        if not members:
+            members = [ctx.author]
+        length_list = []
+        message_string = ""
+        for member in members:
+            random.seed(member.id)
             length = random.randint(0, 20)
-            await ctx.send("**{0}'s size:**\n8{1}D".format(message.author.name, "=" * length))
-        else:
-            user_list = users.split()
-            length_list = []
-            message_string = ""
-            for user in user_list:
-                converter = commands.UserConverter()
-                current_user = await converter.convert(ctx=ctx, argument=user)
-                random.seed(current_user.id)
-                length = random.randint(0, 20)
-                length_list.append({"username": current_user.name, "length": length})
-            for entry in length_list:
-                message_string += "**{0}'s size:**\n8{1}D\n".format(entry["username"], "=" * entry["length"])
-            await ctx.send(message_string)
+            length_list.append({"username": member.nick if member.nick else member.name, "length": length})
+        for entry in length_list:
+            message_string += "**{0}'s size:**\n8{1}D\n".format(entry["username"], "=" * entry["length"])
+        await ctx.send(message_string)
 
 
 def setup(bot):
