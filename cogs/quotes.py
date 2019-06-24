3d46ed704b20971238d44f7cc96cf126bdd3976c
tailoric <eric.schneider96@gmail.com>
2019-06-24 22:00:56 +0200

    fix(quotes) show number and don't shift later quotes after removal

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -4,12 +4,14 @@ from discord.ext import commands
 import asyncio
 import discord
 from typing import Optional
-from random import choice
+from random import randint
+
 class Quotes(commands.Cog):
     """Save and get random quotes provided and added by the users"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.removed_quote = "[Removed Quote]\n"
         with open("data/quotes.txt", "r", encoding="utf-8") as f:
             self.quotes = f.readlines()
 
@@ -17,17 +19,28 @@ class Quotes(commands.Cog):
     async def quote(self, ctx, number: Optional[int], *, quote: Optional[commands.clean_content]):
         """add a quote by writing it down or get a random or specific quote """
         if number and not quote and number-1 < len(self.quotes) and number > 0:
-            await ctx.send(self.quotes[number-1])
+            choice = number-1
+            chosen_quote = self.quotes[choice]
+            if chosen_quote == self.removed_quote:
+                await ctx.send("chosen quote was removed sending random quote instead...")
+            while chosen_quote == self.removed_quote:
+                choice = randint(0, len(self.quotes) -1)
+                chosen_quote = self.quotes[choice]
+            await ctx.send(f"{choice+1}) {chosen_quote}")
             return
         if not quote:
             mes = None
             if number and (number-1 >= len(self.quotes) or number <= 0):
                 mes = await ctx.send("quote number not found sending random quote...")
+            current_quote = self.removed_quote
+            while current_quote == self.removed_quote:
+                choice = randint(0, len(self.quotes)-1)
+                current_quote = self.quotes[choice]
             if mes:
                 await asyncio.sleep(3)
-                await mes.edit(content=choice(self.quotes))
+                await mes.edit(content=f"{choice+1}) {current_quote}")
             else:
-                await ctx.send(choice(self.quotes))
+                await ctx.send(f"{choice+1}) {current_quote}")
             return
         else:
             with open("data/quotes.txt", "a", encoding="utf-8") as f:
@@ -35,7 +48,7 @@ class Quotes(commands.Cog):
                     quote = f"{number} {quote}"
                 f.write(f"{quote}\n")
                 self.quotes.append(f"{quote}\n")
-                await ctx.send("quote added")
+                await ctx.send(f"quote #{len(self.quotes)} added")
 
     @commands.command()
     async def allquotes(self, ctx):
@@ -44,6 +57,8 @@ class Quotes(commands.Cog):
         """
         paginator = commands.Paginator()
         for index, quote in enumerate(self.quotes):
+            if quote == self.removed_quote:
+                continue
             paginator.add_line(f"{index+1}) {quote}")
 
         try:
@@ -60,7 +75,7 @@ class Quotes(commands.Cog):
             await ctx.send(f"Invalid quote number, this number is either too big or smaller than 1, please use a number"
                            f"smaller or equal to {len(self.quotes)}")
             return
-        self.quotes.pop(number-1)
+        self.quotes[number-1] = self.removed_quote
         with open("data/quotes.txt", "w") as f:
             f.writelines(self.quotes)
         await ctx.send(f"quote #{number} deleted")
