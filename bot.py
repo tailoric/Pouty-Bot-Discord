c4662e2dbe9911f86fc2b31038be79d9b966bad7
tailoric <eric.schneider96@gmail.com>
2019-07-06 18:52:00 +0200

    feat(bot) add postgres db to bot

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from cogs.utils.dataIO import DataIO
 import logging
+import asyncpg
 import sys
 import asyncio
 
@@ -16,6 +17,20 @@ bot = commands.Bot(command_prefix=['!', '.'], description=description, owner_id=
 def load_credentials():
     return data_io.load_json("credentials")
 
+async def connect_db_and_start_bot():
+    token = credentials['token']
+    db_info = data_io.load_json("postgres")
+    bot.db = await asyncpg.create_pool(database=db_info['dbname'], user=db_info['user'], password=db_info["password"],
+                                       host="127.0.0.1")
+
+    try:
+        bot.load_extension("cogs.default")
+        await bot.start(token)
+    except KeyboardInterrupt:
+        print("closing connection")
+        await bot.db.close()
+        await bot.logout()
+
 if __name__ == '__main__':
     credentials = load_credentials()
     bot.client_id = credentials['client-id']
@@ -24,7 +39,6 @@ if __name__ == '__main__':
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
-    token = credentials['token']
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(connect_db_and_start_bot())
 
-    bot.load_extension("cogs.default")
-    bot.run(token)
