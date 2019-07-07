6f0b7d3dd46253fc55ba6365d152428945b24627
tailoric <eric.schneider96@gmail.com>
2019-07-07 21:46:25 +0200

    feat(filter) check for forbidden tags on moderator react

--- cogs/filter.py
+++ cogs/filter.py
@@ -1,8 +1,10 @@
 from discord.ext import commands
+import discord
 import json
 import asyncio
 import re
 import os
+from aiohttp import ClientSession
 from .utils.checks import is_owner_or_moderator
 
 class Filter(commands.Cog):
@@ -13,6 +15,8 @@ class Filter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.filter_file_path = "data/filter.json"
+        self.session = ClientSession()
+        self.banned_tags = ['lolicon', 'shotacon']
 
         if os.path.exists(self.filter_file_path):
             with open(self.filter_file_path, "r") as filter_file:
@@ -25,6 +29,9 @@ class Filter(commands.Cog):
             open(self.filter_file_path, 'w').close()
             self.allowed_channel = None
 
+    def cog_unload(self):
+        self.bot.loop.create_task(self.session.close())
+
     @commands.Cog.listener("on_message")
     async def on_message(self, message):
         contains_giphy_or_tenor_link_regex = re.compile("https://(media\.)?(tenor|giphy)?.com/")
@@ -32,6 +39,37 @@ class Filter(commands.Cog):
         if match and not message.channel == self.allowed_channel:
             await asyncio.sleep(1)
             await message.delete()
+
+    async def check_for_tags(self, message):
+        matches = re.findall(r'\b\d{1,6}\b', message)
+        if matches:
+            for match in matches:
+                number = int(match)
+                url = f"https://nhentai.net/api/gallery/{number}"
+                async with self.session.get(url) as response:
+                    if response.status == 200:
+                        data = await response.json()
+                        for tag in data['tags']:
+                            if tag['name'] in self.banned_tags:
+                                return True, tag['name']
+                        return False, None
+
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        mod_role = user.guild.get_role(191094827562041345)
+        search_emoji = self.bot.get_emoji(595953208556257292)
+        if isinstance(user, discord.Member) and mod_role in user.roles and reaction.emoji == search_emoji:
+            check_true, tag = await self.check_for_tags(reaction.message.content)
+            if check_true:
+                await reaction.message.delete()
+                admin_cog = self.bot.get_cog("Admin")
+                if admin_cog and admin_cog.report_channel:
+                    await admin_cog.report_channel.send(f"deleted message by {reaction.message.author.mention} "
+                                                        f"because it contained a banned tag: {tag}")
+
+
+
 
     @is_owner_or_moderator()
     @commands.command(name="filter_exception", pass_context=True)
