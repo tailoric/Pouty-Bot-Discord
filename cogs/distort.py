04133b1ec1f574f5599f07bc8b5853ac00bd3708
tailoric <eric.schneider96@gmail.com>
2019-07-13 16:17:40 +0200

    feat(distort) allow conversion of emotes too

--- cogs/distort.py
+++ cogs/distort.py
@@ -1,9 +1,10 @@
-from discord import File
+from discord import File, PartialEmoji
 from discord.ext import commands
-from typing import Optional
+from typing import Optional, Union
 import aiohttp
 import asyncio
 import io
+import uuid
 import os
 import sys
 
@@ -22,7 +23,7 @@ class Distort(commands.Cog):
         self.bot.loop.create_task(self.session.close())
 
     @commands.group(invoke_without_command=True)
-    async def distort(self, ctx: commands.Context, link: Optional[str]):
+    async def distort(self, ctx: commands.Context, link: Optional[Union[PartialEmoji,str]]):
         """
         creates a distorted version of an image, works with direct upload and
         image link
@@ -30,25 +31,39 @@ class Distort(commands.Cog):
         """
         message = ctx.message
         if message.attachments:
-            url = message.attachments[0].url
+            url = str(message.attachments[0].url)
             filetype = url[url.rfind('.'):]
+            pos = url.rfind("/")
+            filename = url[pos+1:]
             if filetype.lower() not in self.allowed_file_extensions:
                 await ctx.send("not allowed filetype only images or gifs allowed")
                 return
-            await message.attachments[0].save("data/temp" + filetype)
+            await message.attachments[0].save(filename)
         else:
-            async with self.session.get(url=link) as r:
-                filetype = link[link.rfind('.'):]
-                if filetype.lower() not in self.allowed_file_extensions:
-                    await ctx.send("not allowed filetype only images or gifs allowed")
+            if isinstance(link, PartialEmoji):
+                filetype = str(link.url)[str(link.url).rfind("."):]
+                filename = f"{link.name}{filetype}"
+                await link.url.save(f"data/{link.name}{filetype}")
+            else:
+                try:
+                    async with self.session.get(url=link) as r:
+                        filetype = link[link.rfind('.'):]
+                        pos = link.rfind("/")
+                        filename = link[pos+1:]
+                        if filetype.lower() not in self.allowed_file_extensions:
+                            await ctx.send("not allowed filetype only images or gifs allowed")
+                            return
+                        if r.status == 200:
+                            with open(f"data/{filename}", "wb") as f:
+                                buffer = io.BytesIO(await r.read())
+                                f.write(buffer.read())
+                except aiohttp.InvalidURL:
+                    await ctx.send("this command only works with custom emojis")
                     return
-                if r.status == 200:
-                    with open("data/temp" + filetype, "wb") as f:
-                        buffer = io.BytesIO(await r.read())
-                        f.write(buffer.read())
         async with ctx.typing():
-            await self.spawn_magick(filetype)
-            await ctx.send(file=File('data/temp_distort' + filetype))
+            output_file = await self.spawn_magick(filename, filetype)
+            await ctx.send(file=File(output_file))
+            os.remove(output_file)
 
     @distort.command(name="me")
     async def _me(self, ctx):
@@ -58,18 +73,21 @@ class Distort(commands.Cog):
         asset = ctx.author.avatar_url_as(static_format="png")
         filetype = str(asset)[str(asset).rfind("."):str(asset).rfind("?")]
         async with ctx.typing():
-            await asset.save("data/temp" + filetype)
-            await self.spawn_magick(filetype)
-            await ctx.send(file=File("data/temp_distort" + filetype))
+            filename = f"{ctx.author.name}{filetype}"
+            await asset.save(f"data/{filename}")
+            output_path = await self.spawn_magick(filename=filename, filetype=filetype)
+            await ctx.send(file=File(output_path))
 
-    async def spawn_magick(self, filetype):
-        output_path_temp = os.path.join('data', 'temp' + filetype)
-        output_path_distort = os.path.join('data', 'temp_distort' + filetype)
+    async def spawn_magick(self, filename, filetype):
+        output_path_temp = os.path.join('data', filename)
+        output_path_distort = os.path.join('data', str(uuid.uuid4()) + filetype)
         proc = await asyncio.create_subprocess_exec(
             self.image_magick_command, output_path_temp, '-layers', 'coalesce', '-liquid-rescale', '50%x50%',
             '-resize', '200%', output_path_distort
         )
         await proc.communicate()
+        os.remove(output_path_temp)
+        return output_path_distort
 
 
 def setup(bot: commands.Bot):
