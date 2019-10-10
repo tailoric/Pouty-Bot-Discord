dc6056f2e7a846a1a5a30d1b8e9835789e92a63a
tailoric <eric.schneider96@gmail.com>
2019-10-10 16:40:27 +0200

    fix(github) add timeouts to command

--- cogs/github.py
+++ cogs/github.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 from .utils.dataIO import DataIO
+from .utils.checks import channel_only
 import aiohttp
+import asyncio
 class GithubIssues(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -11,6 +13,7 @@ class GithubIssues(commands.Cog):
         self.session = aiohttp.ClientSession()
 
     @commands.command(aliases=['suggest', 'suggestion'])
+    @channel_only(191536772352573440, 336912585960194048, 208765039727869954, 390617633147453444)
     async def proposal(self, ctx, *, title):
         """creates a track able issue on github so the bot creator can easily list and work with suggestions for the
         bot"""
@@ -34,7 +37,11 @@ class GithubIssues(commands.Cog):
         await ctx.send("Please write a description for your suggestion\n"
                        "refer to this guide for the formatting\n"
                        "<https://guides.github.com/features/mastering-markdown/>")
-        message = await self.bot.wait_for('message', check=message_check)
+        try:
+            message = await self.bot.wait_for('message', check=message_check, timeout=5*60.0)
+        except asyncio.TimeoutError:
+            await ctx.send(f"{ctx.author.mention} Timeout reached suggestion was not sent")
+            return
         data['body'] = message.content
         embed = discord.Embed(title=title, description=message.content)
         react_mes = await ctx.send(content="This will be the created issue on github, do you want to send?",
@@ -45,7 +52,11 @@ class GithubIssues(commands.Cog):
         def reaction_check(r, user):
             return user.id == ctx.author.id and isinstance(r.emoji, str) and (r.emoji == reactions['yes'] or
                                                                               r.emoji == reactions['no'])
-        reaction, user = await self.bot.wait_for('reaction_add', check=reaction_check)
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', check=reaction_check, timeout=60.0)
+        except asyncio.TimeoutError:
+            await ctx.send("Timeout reached suggestion was not send")
+            return
         if reaction.emoji == reactions['no']:
             await ctx.send("alright I won't send this suggestion")
             return
