0b88ccbb77323b98504e1c9e972c8a86a8740d54
tailoric <eric.schneider96@gmail.com>
2019-11-04 23:07:29 +0100

    feat(reddit) check for stickied threads on animemes

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -2,7 +2,8 @@ import json
 import aiohttp
 import re
 import datetime
-from discord.ext import commands
+import discord
+from discord.ext import commands, tasks
 from os import path
 from .utils import checks
 
@@ -31,10 +32,98 @@ class Reddit(commands.Cog):
             with open(self.reddit_settings_path, 'r') as f:
                 settings = json.load(f)
                 self.checker_channel = self.bot.get_channel(settings['channel'])
-            self.webhook_user_id = 587605124470276096
+                self.reddit_channel = self.bot.get_channel(settings.get("reddit_channel", None))
+            self.last_stickied_post_time = datetime.datetime.utcnow()
+            self.check_reddit_for_pinned_threads.start()
+            self.bot.loop.create_task(self.create_last_posts_table())
 
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
+        self.check_reddit_for_pinned_threads.stop()
+
+    async def create_last_posts_table(self):
+        async with self.bot.db.acquire() as connection:
+            query = "CREATE TABLE IF NOT EXISTS stickied_threads(" \
+                    "post_id varchar(255) primary key," \
+                    "created timestamp)"
+            async with connection.transaction():
+                await connection.execute(query)
+
+    async def fetch_last_stickied_entries(self):
+        query = "SELECT post_id, created from stickied_threads order by created desc limit 2"
+        async with self.bot.db.acquire() as connection:
+            return await connection.fetch(query)
+    async def insert_post(self, id, created):
+        query = ("INSERT INTO stickied_threads VALUES ($1, $2);"
+                 )
+        query_delete = (" DELETE from stickied_threads WHERE post_id = any (array(select post_id from stickied_threads ORDER BY"
+                 " created desc offset 2))")
+        async with self.bot.db.acquire() as connection:
+            statement = await connection.prepare(query)
+            async with connection.transaction():
+                await statement.fetch(id, created)
+                await connection.execute(query_delete)
+
+
+    @commands.command(name="redditc")
+    @checks.is_owner_or_moderator()
+    async def setup_reddit_channel(self, ctx):
+        """set up the channel for posting stickied threads"""
+        with open(self.reddit_settings_path, 'r+') as f:
+            settings = json.load(f)
+            settings["reddit_channel"] = ctx.channel.id
+            f.seek(0)
+            json.dump(settings, f)
+        self.reddit_channel = ctx.channel
+        await ctx.send(f"{ctx.channel.mention} set up as the reddit channel for announcements")
+
+    @tasks.loop(minutes=1)
+    async def check_reddit_for_pinned_threads(self):
+        async with self.session.get(url="https://reddit.com/r/Animemes.json", auth=self.auth,
+                                    headers=self.headers) as resp:
+            if resp.status == 200:
+                resp_data = await resp.json()
+                stickied_post = [post['data'] for post in resp_data["data"]["children"] if post['data']["stickied"]]
+                last_posts = await self.fetch_last_stickied_entries()
+                if not last_posts:
+                    for post in stickied_post:
+                        post_creation = datetime.datetime.utcfromtimestamp(post['created_utc'])
+                        await self.insert_post(post["id"], post_creation)
+                        return
+                for post in stickied_post:
+                    post_creation = datetime.datetime.utcfromtimestamp(post['created_utc'])
+                    if all([post_creation > old_post["created"]
+                            for old_post in last_posts]):
+                        embed = await self.build_embed_for_stickied_thread(post)
+                        if embed:
+                            await self.reddit_channel.send(embed=embed)
+                            await self.insert_post(post["id"], post_creation)
+
+
+
+    async def build_embed_for_stickied_thread(self, post):
+        async with self.session.get(url="https://reddit.com/r/Animemes/about.json", auth=self.auth,
+                                    headers=self.headers) as resp:
+            if resp.status == 200:
+                resp_data = await resp.json()
+                sub_data = resp_data["data"]
+                if post["is_self"]:
+                    embed = discord.Embed(title=post["title"], timestamp=datetime.datetime.utcfromtimestamp(post["created_utc"]),
+                                          url=post["url"], description=post["selftext"][:500]+"...",
+                                          color=discord.Colour(0xea0027))
+                    embed.set_thumbnail(url=sub_data["header_img"])
+                else:
+                    embed = discord.Embed(title=post["title"], timestamp=datetime.datetime.utcfromtimestamp(post["created_utc"]),
+                                          url=post["url"], color=discord.Colour(0xea0027))
+                    if not post["over_18"]:
+                        embed.set_thumbnail(url=post["thumbnail"])
+                    else:
+                        embed.set_thumbnail(url=sub_data["header_img"])
+
+                embed.set_author(name=post["author"], url=f"https://reddit.com/user/{post['author']}")
+                embed.set_footer(icon_url=sub_data["icon_img"], text="Animemes")
+                return embed
+
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
@@ -75,8 +164,6 @@ class Reddit(commands.Cog):
                                                 re.MULTILINE)
         match = contains_reddit_link_regex.search(message.content)
         url = message.content
-        if message.author.id == self.webhook_user_id:
-            return
         if not match:
             return
         if match.group(1) and match.group(1).startswith('v'):
