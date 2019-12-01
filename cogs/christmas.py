79c97a3325ea03d49d7fe5d198c1d5730b6d4900
tailoric <eric.schneider96@gmail.com>
2019-12-01 13:17:49 +0100

    feat(christmas) make christmas role selectable

--- cogs/christmas.py
+++ cogs/christmas.py
@@ -2,12 +2,28 @@ from discord.ext import commands
 from .utils import checks
 import discord
 import asyncio
+from .utils import dataIO
 
 
 class Christmas(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.green_role = None
+        self.red_role = None
         self.next_role = None
+        self.padoru = discord.utils.get(self.bot.emojis, name="PADORUPADORU")
+        self.communism = discord.utils.get(self.bot.emojis, name="communism")
+        settings_loader = dataIO.DataIO()
+        initial_cogs = settings_loader.load_json("initial_cogs")
+        initial_cogs.append("cogs.christmas")
+        settings_loader.save_json("initial_cogs", initial_cogs)
+
+
+    def cog_unload(self):
+        settings_loader = dataIO.DataIO()
+        initial_cogs = settings_loader.load_json("initial_cogs")
+        initial_cogs.remove("cogs.christmas")
+        settings_loader.save_json("initial_cogs", initial_cogs)
 
     @commands.command(name="christmas", pass_context=True, hidden=True)
     @checks.is_owner_or_moderator()
@@ -16,67 +32,53 @@ class Christmas(commands.Cog):
         starts christmas time
         """
         server = ctx.message.guild
-        red_role, green_role = await self.get_christmas_roles(server)
+        self.red_role, self.green_role = await self.get_christmas_roles(server)
         role = discord.utils.get(server.roles, name="Memester")
-        if(red_role.position < role.position or green_role.position < role.position):
-            await self.bot.move_role(server=server,role=red_role, position=role.position+1)
-            await self.bot.move_role(server=server,role=green_role, position=role.position+2)
-        padoru = discord.utils.get(self.bot.get_all_emojis(), name="PADORUPADORU")
-        padoru_string = "<a:" + padoru.name + ":" + padoru.id + ">"
+        if self.red_role.position < role.position or self.green_role.position < role.position:
+            await self.red_role.edit(position=role.position+1)
+            await self.green_role.edit(position=role.position+1)
         christmas_message = await ctx.send("Christmas time in 3")
         await asyncio.sleep(1)
-        await self.bot.edit_message(message=christmas_message, new_content="Christmas time in 2")
+        await christmas_message.edit(content="Christmas time in 2")
         await asyncio.sleep(1)
-        await self.bot.edit_message(message=christmas_message, new_content="Christmas time in 1")
+        await christmas_message.edit(content="Christmas time in 1")
         await asyncio.sleep(1)
-        await self.bot.edit_message(message=christmas_message,
-                                    new_content="%s MERRY CHRISTMAS %s" % (str(padoru_string), str(padoru_string)))
-        members = sorted([member for member in server.members if role in member.roles], key=lambda m:m.name.lower())
-        for index,member in enumerate(members):
-            if index % 2 == 0:
-                await self.bot.add_roles(member,red_role)
-            else:
-                await self.bot.add_roles(member,green_role)
+        await christmas_message.edit(content=f"{self.padoru} MERRY CHRISTMAS {self.padoru}")
 
     async def get_christmas_roles(self, server):
         role_red = discord.utils.get(server.roles, name="ChristmasSoviets")
         role_green = discord.utils.get(server.roles, name="PadoruPatrol")
         if not role_red and not role_green:
-            role_red = await self.bot.create_role(server=server, name="ChristmasSoviets",color=discord.Color(int("c62f2f",16)))
-            role_green = await self.bot.create_role(server=server, name="PadoruPatrol",color=discord.Color(int("157718",16)))
+            role_red = await server.create_role(name="ChristmasSoviets",color=discord.Color(int("c62f2f",16)))
+            role_green = await server.create_role(name="PadoruPatrol",color=discord.Color(int("157718",16)))
         return role_red, role_green
 
-    @commands.command(pass_context=True, name="next_role")
-    @checks.is_owner_or_moderator()
-    async def _next_role(self, ctx, role: str):
+    @checks.channel_only(191536772352573440, 390617633147453444)
+    @commands.command(name="padoru")
+    async def join_padoru(self, ctx):
         """
-        set the which role needs to be assigned next
-        role: [green|red] set the next role to either green or red
+        join the padoru color squad
         """
-        server = ctx.message.guild
-        role_red = discord.utils.get(server.roles, name="ChristmasSoviets")
-        role_green = discord.utils.get(server.roles, name="PadoruPatrol")
-        if role.lower() == "green":
-            self.next_role = role_green
-        else:
-            self.next_role = role_red
+        if self.green_role is None:
+            self.green_role = discord.utils.get(ctx.guild.roles, name="PadoruPatrol")
+        await ctx.author.add_roles(self.green_role)
+        if self.red_role in ctx.author.roles:
+            await ctx.author.remove_roles(self.red_role)
+        await ctx.send(f"you joined the {self.padoru} squad")
 
-        await ctx.send("set next role to %s" % self.next_role.name)
+    @checks.channel_only(191536772352573440, 390617633147453444)
+    @commands.command(name="soviet")
+    async def join_soviets(self, ctx):
+        """
+        join the christmas soviet squad
+        """
+        if self.red_role is None:
+            self.red_role = discord.utils.get(ctx.guild.roles, name="ChristmasSoviets")
+        await ctx.author.add_roles(self.red_role)
+        if self.green_role in ctx.author.roles:
+            await ctx.author.remove_roles(self.green_role)
+        await ctx.send(f"you joined the {self.communism} squad")
 
-    async def on_member_update(self, before: discord.Member, after: discord.Member):
-        server = before.guild
-        memester = discord.utils.get(server.roles, name="Memester")
-        if before.roles.__contains__(memester) or not after.roles.__contains__(memester):
-            return
-
-        role_green = discord.utils.get(server.roles, name="PadoruPatrol")
-        role_red = discord.utils.get(server.roles, name="ChristmasSoviets")
-        if not self.next_role or self.next_role == role_green:
-            await self.bot.add_roles(after, role_green)
-            self.next_role = role_red
-        else:
-            await self.bot.add_roles(after, role_red)
-            self.next_role = role_green
 
 
 
