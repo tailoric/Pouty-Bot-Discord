84696d07db9386dc7453895a97205e229a278685
tailoric <16943959+tailoric@users.noreply.github.com>
2020-12-05 14:57:04 +0100

    feat(meme_off) add pin and unpin functionality
    Also make it so the bot doesn't discard everything after a cog reload

--- cogs/meme_off.py
+++ cogs/meme_off.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from discord.utils import get
 from .utils import checks
@@ -32,10 +33,16 @@ class MemeOff(commands.Cog):
     """Command suite for Animemes meme-offs only"""
     def __init__(self, bot):
         self.bot = bot
-        self.timer_task = None
-        self.timer_timestamp = None
-        self.submitted_templates = {}
+        if not hasattr(self.bot, 'meme_off_timer'):
+            self.bot.meme_off_timer = None
+        if not hasattr(self.bot, 'meme_off_timer_timestamp'):
+            self.bot.meme_off_timer_timestamp = None
+        if not hasattr(self.bot, 'submitted_templates'):
+            self.bot.submitted_templates = {}
         self.template_order = None
+        if not hasattr(self.bot, 'pinned_template'):
+            self.bot.pinned_template = None
+            self.bot.pinned_by = None
 
     @commands.group(name="meme-off", aliases=["meme_off", "memeoff", "mo"])
     @checks.channel_only("memeoff")
@@ -46,6 +53,12 @@ class MemeOff(commands.Cog):
         if ctx.invoked_subcommand is None:
             await ctx.send_help(self.meme_off)
 
+    @meme_off.command(name="help")
+    async def meme_off_help(self, ctx):
+        """
+        sends this help message
+        """
+        await ctx.send_help(self.meme_off)
     @commands.has_any_role("Subreddit-Senpai", "Discord-Senpai")
     @commands.guild_only()
     @meme_off.command(name="ping")
@@ -69,8 +82,8 @@ class MemeOff(commands.Cog):
         """
         create a timer for the current round [typical inputs are 30 minutes or 60 minutes]
         """
-        if self.timer_task is not None:
-            if not self.timer_task.cancelled() or not self.timer_task.done():
+        if self.bot.meme_off_timer is not None:
+            if not self.bot.meme_off_timer.cancelled() or not self.bot.meme_off_timer.done():
                 return await ctx.send("There is already a timer running. Cancel it with `.meme-off cancel` first")
         if round_duration.endswith("s"):
             round_duration = round_duration[:-1]
@@ -80,15 +93,15 @@ class MemeOff(commands.Cog):
             return await ctx.send(f"No valid time unit the only available units are:\n{', '.join(time_units.keys())}")
         delay = int(amount) * time_units[unit]
         await ctx.send(f"Timer set to {amount} {unit}(s)")
-        self.timer_task = self.bot.loop.create_task(self.timer(delay, ctx))
+        self.bot.meme_off_timer = self.bot.loop.create_task(self.timer(delay, ctx))
 
     @commands.guild_only()
     @meme_off.command(name="deadline", aliases=["dl"])
     async def meme_off_deadline(self, ctx):
         """ see how much time is left until the current deadline is over"""
-        if not self.timer_timestamp:
+        if not self.bot.meme_off_timer_timestamp:
             return await ctx.send("No timer set currently")
-        time_diff = self.timer_timestamp - datetime.utcnow()
+        time_diff = self.bot.meme_off_timer_timestamp - datetime.utcnow()
         minutes, seconds = divmod(time_diff.seconds, 60)
         hours, minutes = divmod(minutes, 60)
         days = time_diff.days
@@ -110,17 +123,21 @@ class MemeOff(commands.Cog):
         """
         cancel the current timer
         """
-        self.timer_task.cancel()
-        self.timer_task = None
-        self.timer_timestamp = None
+        self.bot.meme_off_timer.cancel()
+        self.bot.meme_off_timer = None
+        self.bot.meme_off_timer_timestamp = None
         await ctx.send("Timer was cancelled")
 
     async def timer(self, delay, ctx):
-        self.timer_timestamp = datetime.utcnow() + timedelta(seconds=delay)
+        self.bot.meme_off_timer_timestamp = datetime.utcnow() + timedelta(seconds=delay)
         await asyncio.sleep(delay)
         await ctx.send("Round has finished now.")
-        self.timer_task = None
-        self.timer_timestamp = None
+        if self.bot.pinned_template:
+            await self.bot.pinned_template.unpin()
+        self.bot.pinned_template = None
+        self.bot.pinned_by = None
+        self.bot.meme_off_timer = None
+        self.bot.meme_off_timer_timestamp = None
 
     @commands.dm_only()
     @meme_off.command("submit")
@@ -129,20 +146,20 @@ class MemeOff(commands.Cog):
         **ONLY IN DMs:** add a template to the random rotation, file needs to be attached for this command
         """
         if ctx.message.attachments:
-            template_submission = self.submitted_templates.get(ctx.author.id, None)
+            template_submission = self.bot.submitted_templates.get(ctx.author.id, None)
             if not template_submission:
                 template_submission = TemplateSubmission(ctx.author.id)
 
             template_submission.add_template(ctx.message.attachments[0].url)
-            self.submitted_templates[ctx.author.id] = template_submission
+            self.bot.submitted_templates[ctx.author.id] = template_submission
             await ctx.send("template successfully submitted")
         elif link:
-            template_submission = self.submitted_templates.get(ctx.author.id, None)
+            template_submission = self.bot.submitted_templates.get(ctx.author.id, None)
             if not template_submission:
                 template_submission = TemplateSubmission(ctx.author.id)
 
             template_submission.add_template(link)
-            self.submitted_templates[ctx.author.id] = template_submission
+            self.bot.submitted_templates[ctx.author.id] = template_submission
             await ctx.send("template successfully submitted")
         else:
             return await ctx.send("You need to attach a file to your message or provide a link")
@@ -151,29 +168,64 @@ class MemeOff(commands.Cog):
     @meme_off.command(name="template", aliases=["temp"])
     async def meme_off_template(self, ctx):
         """get a random submission from the template rotation"""
+        if self.bot.pinned_template:
+            return await ctx.send("A template is currently pinned remove it before getting a new one")
         if not self.template_order:
-            if not self.submitted_templates:
+            if not self.bot.submitted_templates:
                 return await ctx.send("No templates submitted")
-            self.template_order = [u for u in self.submitted_templates.keys()]
+            self.template_order = [u for u in self.bot.submitted_templates.keys()]
             random.shuffle(self.template_order)
-        submission = self.submitted_templates[self.template_order.pop()]
+        submission = self.bot.submitted_templates[self.template_order.pop()]
         template = submission.get_template()
         while not template:
             if len(self.template_order) == 0:
                 break
-            submission = self.submitted_templates[self.template_order.pop()]
+            submission = self.bot.submitted_templates[self.template_order.pop()]
             template = submission.get_template()
         if not template:
             return await ctx.send("no templates left")
-        await ctx.send(f"Template for this round from <@{submission.user_id}> is:\n{template}")
+        self.bot.pinned_template = await ctx.send(f"Template for this round from <@{submission.user_id}> is:\n{template}")
+        await self.bot.pinned_template.pin()
+        self.bot.pinned_by = ctx.author
+
 
     @commands.has_any_role("Subreddit-Senpai", "Discord-Senpai")
     @commands.guild_only()
     @meme_off.command(name="delete_templates", aliases=["deltemplates", "delTemplates", "delete"])
     async def meme_off_templates_reset(self, ctx):
         """ remove all template submissions"""
-        self.submitted_templates = {}
+        self.bot.submitted_templates = {}
         await ctx.send("all templates removed")
+
+    @commands.guild_only()
+    @meme_off.command(name="pin", aliases=[])
+    async def meme_off_pin(self, ctx, message : discord.Message):
+        """
+        provide a message link to pin as template
+        """
+        if self.bot.pinned_template:
+            return await ctx.send("There already is a template pinned!")
+        await message.pin()
+        self.bot.pinned_template = message
+        self.bot.pinned_by = ctx.author
+
+    @commands.guild_only()
+    @meme_off.command(name="unpin", aliases=[])
+    async def meme_off_unpin(self, ctx):
+        """
+        unpin the last template (only usable by moderators or the user who pinned the template)
+        """
+        if not self.bot.pinned_template:
+            return await ctx.send("There is no template pinned right now")
+        role_names = [r.name for r in ctx.author.roles]
+        if ctx.author == self.bot.pinned_by or "Discord-Senpai" in role_names or "Subreddit-Senpai" in role_names:
+            await self.bot.pinned_template.unpin()
+            self.bot.pinned_template = None
+            self.bot.pinned_by = None
+            await ctx.send("template unpinned")
+        else:
+            await ctx.send("Only a moderator or the user who pinned the template can unpin it")
+
 
 def setup(bot):
     bot.add_cog(MemeOff(bot))
