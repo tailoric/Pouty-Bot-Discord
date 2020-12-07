d539cb21f9de6ce348d6c70ef6eca2a9ed7f8c02
tailoric <16943959+tailoric@users.noreply.github.com>
2020-12-07 17:54:27 +0100

    fix(meme_off) disallow negative amounts for timer

--- cogs/meme_off.py
+++ cogs/meme_off.py
@@ -89,6 +89,8 @@ class MemeOff(commands.Cog):
             round_duration = round_duration[:-1]
         time_units = {"hour": 3600, "minute": 60, "second": 1}
         amount, unit = round_duration.split(" ")
+        if amount < 0: 
+            return await ctx.send("Only positive values allowed")
         if unit not in time_units.keys():
             return await ctx.send(f"No valid time unit the only available units are:\n{', '.join(time_units.keys())}")
         delay = int(amount) * time_units[unit]
