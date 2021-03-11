a1828c23b7926ee49109315c53b3dd6a333df81f
tailoric <16943959+tailoric@users.noreply.github.com>
2021-03-11 19:58:48 +0100

    feat(thread) add thread list channel

--- cogs/thread.py
+++ cogs/thread.py
@@ -36,7 +36,8 @@ class Thread(commands.Cog):
                         "category_channel": None,
                         "join_reaction" : "\N{OPEN LOCK}",
                         "attachments_backlog": None,
-                        "livetime": 24
+                        "livetime": 24,
+                        "thread_list_channel": None
                         }
                 json.dump(self.settings,f)
 
@@ -87,7 +88,8 @@ class Thread(commands.Cog):
             id SERIAL PRIMARY KEY,
             invocation_channel_id BIGINT NOT NULL,
             invocation_message_id BIGINT NOT NULL,
-            thread_channel_id BIGINT NOT NULL
+            thread_channel_id BIGINT NOT NULL,
+            copy_message_id BIGINT NOT NULL
         )
         """)
 
@@ -104,16 +106,19 @@ class Thread(commands.Cog):
         if not category:
             return await ctx.send(f"No category for channel creation set please use `{ctx.prefix}thread category` to set it")
         disc_channel = await category.create_text_channel(name=topic, topic=self.topic)
+        thread_list_channel = self.bot.get_channel(self.settings.get("thread_list_channel"))
         await disc_channel.set_permissions(ctx.author, read_messages=True)
         embed = discord.Embed(title=f"Thread: {topic} \N{OPEN LOCK}", 
                 description=f"To join the channel {disc_channel.mention} react with {self.settings.get('join_reaction')}\nremove reaction to leave", 
                 colour=discord.Colour(0x76d16a))
         thread_start = await ctx.send(embed=embed)
+        thread_list_copy = await thread_list_channel.send(embed=embed)
         await disc_channel.send(textwrap.dedent(self.thread_rule.format(topic)))
         await thread_start.add_reaction(self.settings.get('join_reaction'))
+        await thread_list_copy.add_reaction(self.settings.get('join_reaction'))
         await self.bot.db.execute("""
-            INSERT INTO thread_channels (invocation_channel_id, invocation_message_id, thread_channel_id) VALUES ($1, $2, $3)
-        """, ctx.channel.id, thread_start.id, disc_channel.id)
+            INSERT INTO thread_channels (invocation_channel_id, invocation_message_id, thread_channel_id, copy_message_id) VALUES ($1, $2, $3, $4)
+        """, ctx.channel.id, thread_start.id, disc_channel.id, thread_list_copy.id)
 
 
     @is_owner_or_moderator()
@@ -127,6 +132,18 @@ class Thread(commands.Cog):
             json.dump(self.settings, s)
         await ctx.send(f"channel set to category: {category.name}")
 
+    @is_owner_or_moderator()
+    @thread.command(name="list")
+    async def thread_set_list(self, ctx, channel: discord.TextChannel):
+        """
+        Set the channel which will host the thread list
+        """
+        with self.settings_path.open("w") as s:
+            self.settings["thread_list_channel"] = channel.id
+            json.dump(self.settings, s)
+        await ctx.send(f"set thread list channel to: {channel.mention}")
+
+    
     @is_owner_or_moderator()
     @thread.command(name="livetime", aliases=["lt"])
     async def thread_livetime(self, ctx, livetime: int=None):
@@ -148,21 +165,23 @@ class Thread(commands.Cog):
         """
         Close a thread channel before its livetime is over
         """
-        thread_info = await self.bot.db.fetchrow("SELECT thread_channel_id, invocation_channel_id, invocation_message_id FROM thread_channels WHERE thread_channel_id = $1", thread.id)
+        thread_info = await self.bot.db.fetchrow("SELECT thread_channel_id, invocation_channel_id, invocation_message_id, copy_message_id FROM thread_channels WHERE thread_channel_id = $1", thread.id)
         if not thread_info:
             return await ctx.send(f"The channel {thread.mention} is not a thread channel")
         thread_channel = self.bot.get_channel(thread_info.get("thread_channel_id"))
         if thread_channel:
+            thread_list_channel = self.bot.get_channel(self.settings.get("thread_list_channel"))
+            copy_message = thread_list_channel.get_partial_message(thread_info.get("copy_message_id"))
             invocation_channel = self.bot.get_channel(thread_info.get("invocation_channel_id"))
             message = invocation_channel.get_partial_message(thread_info.get("invocation_message_id"))
-            await self.delete_thread(thread_channel, message)
+            await self.delete_thread(thread_channel, message, copy_message)
 
     @tasks.loop(hours=1)
     async def check_activity(self):
         """
         iterate through open threads and check if they are inactive and can be deleted
         """
-        open_threads = await self.bot.db.fetch("SELECT thread_channel_id, invocation_message_id, invocation_channel_id FROM thread_channels")
+        open_threads = await self.bot.db.fetch("SELECT thread_channel_id, invocation_message_id, invocation_channel_id, copy_message_id FROM thread_channels")
         for thread in open_threads:
             thread_channel = self.bot.get_channel(thread.get("thread_channel_id"))
             last_message = next(iter(await thread_channel.history(limit=1).flatten()), None)
@@ -177,17 +196,21 @@ class Thread(commands.Cog):
                     delete_channel = True
 
             if delete_channel:
+                thread_list_channel = self.bot.get_channel(self.settings.get("thread_list_channel"))
+                copy_message = thread_list_channel.get_partial_message(thread.get("copy_message_id"))
                 invocation_channel = self.bot.get_channel(thread.get("invocation_channel_id"))
                 message = invocation_channel.get_partial_message(thread.get("invocation_message_id"))
-                await self. delete_thread(thread_channel, message)
+                await self.delete_thread(thread_channel, message, copy_message)
 
-    async def delete_thread(self, thread_channel, thread_message):
+    async def delete_thread(self, thread_channel, thread_message, copy_message):
         chat_log = await self.generate_file(thread_channel)
         chat_log_message = await self.attachments_backlog.send(file=discord.File(chat_log, filename=f"{thread_channel.name}.txt"))
         embed = discord.Embed(title=f"Thread: {thread_channel.name.replace('-',' ')} \N{LOCK}", description="channel closed see the below text file for a chat log", colour=discord.Colour(0xd16a76))
         embed.add_field(name="Chat log", value=f"[{thread_channel.name}.txt]({chat_log_message.attachments[0].url})")
         await thread_message.edit(embed=embed)
+        await copy_message.edit(embed=embed)
         await thread_message.clear_reactions()
+        await copy_message.clear_reactions()
         await thread_channel.delete()
         await self.bot.db.execute(""" DELETE FROM thread_channels WHERE thread_channel_id = $1""", thread_channel.id)
 
@@ -200,7 +223,7 @@ class Thread(commands.Cog):
             return
         if self.admin_cog and self.admin_cog.mute_role in payload.member.roles:
             return
-        thread_channel = await self.bot.db.fetchval("""SELECT thread_channel_id FROM thread_channels WHERE invocation_message_id = $1""", payload.message_id)
+        thread_channel = await self.bot.db.fetchval("""SELECT thread_channel_id FROM thread_channels WHERE invocation_message_id = $1 OR copy_message_id = $1""", payload.message_id)
         if thread_channel:
             thread_channel = self.bot.get_channel(thread_channel)
             await thread_channel.set_permissions(payload.member, read_messages=True)
@@ -212,7 +235,7 @@ class Thread(commands.Cog):
         """
         if payload.user_id == self.bot.user.id or payload.emoji.name != self.settings.get('join_reaction') or not payload.guild_id:
             return
-        thread_channel = await self.bot.db.fetchval("""SELECT thread_channel_id FROM thread_channels WHERE invocation_message_id = $1""", payload.message_id)
+        thread_channel = await self.bot.db.fetchval("""SELECT thread_channel_id FROM thread_channels WHERE invocation_message_id = $1 OR copy_message_id = $1""", payload.message_id)
         if thread_channel:
             thread_channel = self.bot.get_channel(thread_channel)
             member = thread_channel.guild.get_member(payload.user_id)
