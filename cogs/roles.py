004d5ec1aff584f49b48f25bc0759b96b72f0ac1
Eric Schneider <eric.schneider96@gmail.com>
2021-10-01 21:31:19 +0200

    feat(roles) add role icon to roleinfo

--- cogs/roles.py
+++ cogs/roles.py
@@ -248,6 +248,8 @@ class Roles(commands.Cog):
             embed.add_field(name="Colour", value=role.colour)
             embed.set_footer(text="Role was created on")
             embed.timestamp = role.created_at
+            if role.icon: 
+                embed.set_thumbnail(role.icon.url)
             if description:
                 embed.description = description
         await ctx.send(embed=embed)
