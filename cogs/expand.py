893c77913396610a471d57ad55f38dca2ec0d30a
tailoric <16943959+tailoric@users.noreply.github.com>
2022-04-04 09:50:51 +0200

    fix(expand) remove debug print

--- cogs/expand.py
+++ cogs/expand.py
@@ -244,7 +244,6 @@ class LinkExpander(commands.Cog):
 
         post_data = {}
         headers = {'User-Agent': 'https://github.com/tailoric/Pouty-Bot-Discord Pouty-Bot by /u/Saikimo'}
-        print(reddit_request)
         resp = await self.httpx.get(url=reddit_request, headers=headers, follow_redirects=True)
         resp.raise_for_status()
         post_data = resp.json()
