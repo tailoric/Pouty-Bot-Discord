34c9bfb74e3b26867dd9829a1167ac34587e5309
tailoric <16943959+tailoric@users.noreply.github.com>
2022-04-05 22:59:02 +0200

    WIP rewrite poll command

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,247 +1,179 @@
 from datetime import datetime, timedelta
 from discord.ext import commands, tasks
-from discord import Embed, Message, NotFound, Forbidden, HTTPException
+from discord import Embed, Message, NotFound, Forbidden, HTTPException, app_commands
+import discord
 from discord.utils import find, utcnow
 from .utils.checks import is_owner_or_moderator
 from .utils.converters import ReferenceOrMessage, TimeConverter
-from typing import Optional, List
+from typing import Literal, Optional, List, TypedDict, Union, Sequence
 import logging
 import re
+import asyncpg
+from uuid import UUID, uuid4
+from dataclasses import dataclass, field
 
 timing_regex = re.compile(r"^(?P<days>\d+\s?d(?:ay)?s?)?\s?(?P<hours>\d+\s?h(?:our)?s?)?\s?(?P<minutes>\d+\s?m(?:in(?:ute)?s?)?)?\s?(?P<seconds>\d+\s?s(?:econd)?s?)?")
 
 
-class PollFlags(commands.FlagConverter):
-    title: str
-    duration: TimeConverter
-    options: List[str] = commands.flag(name="option", aliases=['o'], max_args=10)
+@dataclass
+class PollOption:
+    id: UUID
+    text: str
+    
+@dataclass
+class PollVote:
+    id: UUID
+    user: Union[int, discord.User]
+    option: PollOption
 
+@dataclass
+class PollData:
+    id: UUID
+    title: str
+    type: Literal["single","multi"]
+    channel: Union[int, discord.TextChannel, discord.VoiceChannel]
+    guild: Union[int, discord.Guild]
+    creator: Union[int, discord.User, discord.Member]
+    end_date: datetime
+    message: Union[int, discord.Message, None] = None
+    options: List[PollOption] = field(default_factory=list)
+    votes: dict = field(default_factory=dict)
+
+    async def add_vote(self, vote: PollVote):
+        existing_votes = self.votes.get(vote.user.id)
+        if existing_votes and self.type == "single":
+            self.votes[vote.user.id] = [vote]
+        elif existing_votes and self.type == "multi" and vote.option.id not in [ex.option.id for ex in existing_votes]:
+            self.votes[vote.user.id].append(vote)
+        elif existing_votes is None:
+            self.votes[vote.user.id] = [vote]
+            
+
+
+class PollOptionSelect(discord.ui.Select):
+    def __init__(self, *, poll: PollData) -> None:
+        self.poll = poll
+        self.is_multi = poll.type == "multi"
+        max_values = 1
+        if self.is_multi:
+            max_values = len(poll.options) - 1
+        super().__init__(placeholder="Choose an option to vote on", max_values=max_values, row=0)
+        for option in poll.options:
+            self.add_option(label=option.text, value=str(option.id))
+
+    async def callback(self, interaction: discord.Interaction):
+        print(self.poll.options)
+        for selection in self.values:
+            print(selection)
+            option = next(filter(lambda o: o.id == UUID(selection) , self.poll.options))
+            await self.poll.add_vote(PollVote(id=uuid4(), user=interaction.user, option=option))
+        await interaction.response.send_message(content=f"Voted for {[v.option.text for v in self.poll.votes[interaction.user.id]]}")
+
+
+class PollView(discord.ui.View):
+    def __init__(self, *, poll: PollData):
+        super().__init__(timeout=None)
+        self.options = []
+        self.select = PollOptionSelect(poll=poll)
+        self.add_item(self.select)
+
+    @discord.ui.button(label="Submit", style=discord.ButtonStyle.blurple, row=1)
+    async def submit_vote(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(f"Vote counted for `{self.select.values}`", ephemeral=True)
+
+class PollModal(discord.ui.Modal):
+
+    def __init__(self, *, bot: commands.Bot, num_options, poll_data: PollData) -> None:
+        self.poll_data = poll_data
+        self.num_options = num_options
+        self.is_multi = poll_data.type == "multi"
+        self.bot = bot
+        super().__init__(title=poll_data.title, timeout=None)
+        for i in range(1, num_options+1):
+            txt_input = discord.ui.TextInput(label=f"Option {i}")
+            self.add_item(txt_input)
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        embed = discord.Embed(title=self.title)
+        for idx,txt_input in enumerate(self.children):
+            self.poll_data.options.append(PollOption(uuid4(), txt_input.value))
+            embed.add_field(name=idx+1, value=txt_input.value, inline=False)
+        await interaction.response.send_message(embed=embed, view=PollView(poll=self.poll_data))
+        self.poll_data.message = await interaction.original_message()
+        await self.bot.db.execute('''
+        INSERT INTO poll.data VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        ''', self.poll_data.id, self.poll_data.channel.id, self.poll_data.message.id, self.poll_data.guild.id, self.poll_data.creator.id, self.poll_data.type, self.poll_data.title, self.poll_data.end_date)
+        await self.bot.db.executemany('''
+        INSERT INTO poll.option VALUES ($1, $2, $3)
+        ''', [(opt.id, opt.text, self.poll_data.id) for opt in self.poll_data.options]
+        )
 class Poll(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.option_labels = [
-                    "\N{DIGIT ONE}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{DIGIT TWO}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{DIGIT THREE}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{DIGIT FOUR}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{DIGIT FIVE}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{DIGIT SIX}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{DIGIT SEVEN}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{DIGIT EIGHT}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{DIGIT NINE}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}",
-                    "\N{KEYCAP TEN}"
-                ]
 
     async def cog_load(self):
-        self.bot.loop.create_task(self.create_database())
-        self.check_polls.start()
+        await self.create_database()
     async def cog_unload(self):
-        self.check_polls.cancel()
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
-        poll = await self.fetch_poll(payload.message_id)
-        if not poll:
-            return
-        guild = self.bot.get_guild(payload.guild_id)
-        if payload.user_id == guild.me.id:
-            return
-        channel = self.bot.get_channel(payload.channel_id)
-        message = await channel.fetch_message(payload.message_id)
-        for reaction in message.reactions:
-            if reaction.emoji not in self.option_labels:
-                await reaction.clear()
-                continue
-            if reaction.emoji == payload.emoji.name:
-                continue
-            user = next(iter([u async for u in reaction.users() if u.id == payload.member.id]), None)
-            if user and not poll.get("multi"):
-                await reaction.remove(user)
+        pass
 
     @tasks.loop(seconds=5.0)
     async def check_polls(self):
-        polls = await self.fetch_polls()
-        if not polls:
-            return
-        for poll in polls:
-            if poll.get("end_ts") > utcnow():
-                continue
-            try:
-                poll_channel = self.bot.get_channel(poll.get("channel_id"))
-                poll_msg = await poll_channel.fetch_message(poll.get("message_id"))
-                reactions = poll_msg.reactions
-                options = poll_msg.embeds[0].description.splitlines()
-                reactions = sorted(reactions, key=lambda r: r.count, reverse=True)
-                valid_reacts = list(filter(lambda r: r.emoji in self.option_labels, reactions))
-                top_count = valid_reacts[0].count
-                winners = list(filter(lambda r: r.count == top_count , valid_reacts))
-                embed = poll_msg.embeds[0]
-                winner_text = [t for t in options for w in winners if t.startswith(w.emoji)]
-                win_embed = Embed(title="Poll finished")
-                win_embed.add_field(name="Title", value=poll_msg.embeds[0].title, inline=False)
-                for w in winner_text:
-                    win_embed.add_field(name="Winner", value=w[5:])
-                    embed.add_field(name="Winner", value=w[5:])
-                win_embed.add_field(name="jump to", value=f"[poll]({poll_msg.jump_url})", inline=False)
-                await poll_channel.send(embed=win_embed)
-                await poll_msg.edit(embed=embed)
-                await self.delete_poll(poll_msg.id)
-            except (NotFound, Forbidden, HTTPException) as e:
-                logger = logging.getLogger("PoutyBot")
-                logger.error("error in the poll loop, message_id: %s, channel_id: %s ",poll.get("message_id"), poll.get("channel_id"),exc_info=1)
-                await self.delete_poll(poll.get("message_id"))
-            except:
-                logger = logging.getLogger("PoutyBot")
-                logger.error("error in the poll loop, message_id: %s, channel_id: %s ",poll.get("message_id"), poll.get("channel_id"),exc_info=1)
+        pass
 
 
     async def create_database(self):
         query = """
-            CREATE TABLE IF NOT EXISTS polls (
-                message_id BIGINT PRIMARY KEY,
+            CREATE SCHEMA IF NOT EXISTS poll;
+            CREATE TABLE IF NOT EXISTS poll.data (
+                poll_id UUID PRIMARY KEY,
                 channel_id BIGINT,
-                end_ts TIMESTAMP WITH TIME ZONE,
-                multi BOOLEAN
-                )
+                message_id BIGINT,
+                guild_id BIGINT,
+                creator_id BIGINT,
+                type TEXT,
+                title TEXT,
+                end_date TIMESTAMP WITH TIME ZONE
+            );
+            CREATE TABLE IF NOT EXISTS poll.option(
+                option_id UUID PRIMARY KEY,
+                text TEXT,
+                poll UUID REFERENCES poll.data (poll_id) ON DELETE CASCADE
+            );
+            CREATE TABLE IF NOT EXISTS poll.vote (
+                vote_id UUID PRIMARY KEY,
+                user_id BIGINT,
+                option UUID REFERENCES poll.option (option_id) ON DELETE CASCADE,
+                poll UUID REFERENCES poll.data (poll_id) ON DELETE CASCADE
+            );
         """
         await self.bot.db.execute(query)
 
-    async def insert_poll(self, message_id: int, channel_id: int, end: datetime, multi=False):
-        async with self.bot.db.acquire() as con:
-            query = """
-                INSERT INTO polls (message_id, channel_id, end_ts, multi)VALUES ($1, $2, $3, $4)
-            """
-            statement = await con.prepare(query)
-            async with con.transaction():
-                await statement.fetch(message_id, channel_id, end, multi)
+    poll = app_commands.Group(name="poll", description="Commands for creating polls")
 
-    async def fetch_poll(self, message_id):
-        async with self.bot.db.acquire() as con:
-            query = """
-                SELECT message_id, channel_id, end_ts, multi
-                FROM polls
-                WHERE message_id = $1
-            """
-            statement = await con.prepare(query)
-            async with con.transaction():
-                return await statement.fetchrow(message_id)
+    @poll.command(name="single")
+    @app_commands.describe(title="The title of the Poll")
+    @app_commands.describe(options="Number of poll options")
+    async def poll_single(self, interaction: discord.Interaction, title: str, options: app_commands.Range[int, 2, 25]):
+        """
+        create a single choice poll
+        """
+        poll_data = PollData(uuid4(), title=title, channel=interaction.channel, guild=interaction.guild, creator=interaction.user, type="single", end_date=discord.utils.utcnow() + timedelta(minutes=5))
+        poll = (PollModal(bot=self.bot, poll_data=poll_data, num_options=options))
+        await interaction.response.send_modal(poll)
 
-    async def fetch_polls(self):
-        async with self.bot.db.acquire() as con:
-            query = """
-                SELECT message_id, channel_id, end_ts
-                FROM polls
-            """
-            statement = await con.prepare(query)
-            async with con.transaction():
-                return await statement.fetch()
-
-    async def delete_poll(self, message_id):
-        async with self.bot.db.acquire() as con:
-            query = """
-                DELETE FROM polls WHERE message_id = $1
-            """
-            statement = await con.prepare(query)
-            async with con.transaction():
-                await statement.fetch(message_id)
-
-    @commands.group(invoke_without_command=True, usage="poll `title:` Title of Poll `duration:` 24h `option:` option 1 `o:` option 2")
-    async def poll(self, ctx, *, flags: PollFlags):
+    @poll.command(name="multi")
+    @app_commands.describe(title="The title of the Poll")
+    @app_commands.describe(options="Number of poll options")
+    async def poll_multi(self, interaction: discord.Interaction, title: str, options: app_commands.Range[int, 3, 25]):
         """
-        create a single choice poll (at most 10 choices possible)
-        **flags**:
-        `title:` The title of the poll
-        `duration:` the duration of the poll (`2 days 4 hours` for example) can accept days hours minutes and seconds
-        `o[ption]:` the poll options flag can be written short with `o:` every option must preceed with this flag
-        example `option: option number 1 o: option number 2`
-
+        create a single choice poll
         """
-        if ctx.invoked_subcommand:
-            return
-        end_timestamp = flags.duration
-        if not end_timestamp:
-            return await ctx.send("incorrect time format: a valid example is `2d20h30m10s` for 2 days 20 hours 30 minutes and 10 seconds")
-        if len(flags.options) > 10:
-            return await ctx.send("No more than 10 choices allowed!")
-        description = ''
-        reactions = []
-        for index, option in enumerate(flags.options):
-            reactions.append(self.option_labels[index])
-            description += f"{self.option_labels[index]}: {option}\n"
-        embed = Embed(
-                    title=flags.title,
-                    description=description
-                )
-        poll_msg = await ctx.send(embed=embed)
-        for reaction in reactions:
-            await poll_msg.add_reaction(reaction)
-        await self.insert_poll(poll_msg.id, poll_msg.channel.id, end_timestamp)
-
-    @poll.command(usage="poll multi `title:` Title of Poll `duration:` 24h `option:` option 1 `o:` option 2")
-    async def multi(self, ctx, title, * ,flags: PollFlags):
-        """
-        create a multiple choice poll
-        """
-        if ctx.invoked_subcommand:
-            return
-        end_timestamp = flags.duration
-        if not end_timestamp:
-            return await ctx.send("incorrect time format: a valid example is `2d20h30m10s` for 2 days 20 hours 30 minutes and 10 seconds")
-        if len(flags.options) > 10:
-            return await ctx.send("No more than 10 choices allowed!")
-        description = ''
-        reactions = []
-        for index, option in enumerate(flags.options):
-            reactions.append(self.option_labels[index])
-            description += f"{self.option_labels[index]}: {option}\n"
-        embed = Embed(
-                    title=title,
-                    description=description
-                )
-        poll_msg = await ctx.send(embed=embed)
-        for reaction in reactions:
-            await poll_msg.add_reaction(reaction)
-        await self.insert_poll(poll_msg.id, poll_msg.channel.id, end_timestamp, True)
-    @is_owner_or_moderator()
-    @poll.command()
-    async def delete(self, ctx, message: Optional[Message]):
-        """
-        delete a poll by providing a message id of a poll message
-        """
-        if not message:
-            converter = ReferenceOrMessage()
-            try:
-                message = await converter.convert(ctx, message)
-            except:
-                return await ctx.send("please provide a valid jump url or reply to a poll message")
-        poll = await self.fetch_poll(message.id)
-        if not poll:
-            return await ctx.send("Message was not a poll")
-        poll_channel = self.bot.get_channel(poll.get("channel_id"))
-        poll_msg = await poll_channel.fetch_message(poll.get("message_id"))
-        await poll_msg.delete()
-        await self.delete_poll(poll_msg.id)
-        await ctx.send("poll deleted")
+        poll_data = PollData(uuid4(), title=title, channel=interaction.channel, guild=interaction.guild, creator=interaction.user, type="multi", end_date=discord.utils.utcnow() + timedelta(minutes=5))
+        poll = (PollModal(bot=self.bot, poll_data=poll_data, num_options=options))
+        await interaction.response.send_modal(poll)
 
     
-    @poll.command()
-    async def when(self, ctx, message: Optional[Message]):
-        """
-        check how long until a poll is finished by providing the message id of a poll message
-        """
-        if not message:
-            converter = ReferenceOrMessage()
-            try:
-                message = await converter.convert(ctx, message)
-            except:
-                return await ctx.send("please provide a valid jump url or reply to a poll message")
-        poll = await self.fetch_poll(message.id)
-        if not poll:
-            return await ctx.send("Message was not a poll")
-        end = poll.get("end_ts")
-        await ctx.send(str(end - utcnow()))
-
 
 async def setup(bot):
     await bot.add_cog(Poll(bot))
