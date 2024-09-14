40284ae50fe40f85573aa72b4e0b9bee913739df
tailoric <16943959+tailoric@users.noreply.github.com>
2024-09-14 22:32:17 +0000

    chore(image_search) deprecate youtube command

--- cogs/image_search.py
+++ cogs/image_search.py
@@ -331,44 +331,9 @@ class Search(commands.Cog):
                     for page in paginator.pages:
                         await ctx.send(page)
 
-    @commands.command()
-    async def yt_version(self, ctx):
-        await ctx.send(youtube_dl.version.__version__)
     @commands.command(aliases=["y",'yt'])
     async def youtube(self,  ctx,*, query: str):
-        try:
-            ytdl = yt_dlp.YoutubeDL({"quiet": True})
-            async with ctx.typing():
-                info = ytdl.extract_info("ytsearch: " + query, download=False)
-                url = info["entries"][0]["webpage_url"]
-            await ctx.send(url)
-        except (yt_dlp.utils.DownloadError, yt_dlp.utils.ExtractorError) as yt_error:
-            self.logger.error(yt_error, exc_info=1)
-            await ctx.send("Youtube dl seems to be outdated please call `update_ytdl`, "
-                           "if that doesn't fix the problem change your search or contact the bot owner")
-        except Exception as e:
-            await ctx.send(f"Command exited with error: ```\n{e}\n```")
-            self.logger.error(e, exc_info=1)
-
-    
-    @commands.command(name="update_ytdl")
-    async def update_ytdl(self, ctx):
-        async with ctx.typing():
-            try: 
-                proc = await asyncio.create_subprocess_exec(
-                        sys.executable, '-m', 'pip', 'install', '-U', 'yt_dlp'
-                        )
-                await proc.communicate()
-                await ctx.send("update completed")
-                keys = sys.modules.copy().keys()
-                for key in keys:
-                    if type(key) is str and key.startswith('yt_dlp'):
-                        del sys.modules[key]
-
-                self.bot.reload_extension('cogs.image_search')
-            except Exception as e:
-                self.logger.error(e, exc_info=1)
-                raise e
+        await ctx.send("This command does not work anymore and is going to be deleted soon.")
 
     @commands.command()
     async def google(self,  ctx, *, query: str):
